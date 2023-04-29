0a9c0e70640ed06ce06b74237935b1a963e4e219
nickodaken <nickodaken24@gmail.com>
2023-04-29 16:44:29 +0800

    data carousel, post, product, testimonies for landingpage

--- app/Http/Controllers/Web/LandingController.php
+++ app/Http/Controllers/Web/LandingController.php
@@ -4,11 +4,21 @@ namespace App\Http\Controllers\Web;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Carousel;
+use App\Models\Blog\Post;
+use App\Models\Product;
+use App\Models\Testimony;
 
 class LandingController extends Controller
 {
     public function index()
     {
-        return view('landing.index');
+        $carousels = Carousel::where('status', true)->paginate(10);
+        $posts = Post::where('is_published', true)->paginate(10);
+        $products = Product::where('status', true)->paginate(10);
+        $testimonies = Testimony::where('status', true)->paginate(10);
+
+        $data = compact('carousels', 'posts', 'products', 'testimonies');
+        return view('landing.index', $data);
     }
 }

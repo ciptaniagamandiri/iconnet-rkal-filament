1e2398b97c6e92db141e0e46a8d5ff0fa829dccb
msbarf14 <misbahularifin14@gmail.com>
2023-05-19 22:14:07 +0800

    passing data carousel components

--- resources/views/components/carousel.blade.php
+++ resources/views/components/carousel.blade.php
@@ -1,17 +1,33 @@
+@php
+    use App\Models\Carousel;
+
+    $carousel = Carousel::where('status', 1)->get();
+    $carousel->transform(function($item) {
+        return [
+            'id' => $item['id'],
+            'title' => $item['title'],
+            'path' => sprintf('/storage/%s', $item['file']),
+        ];
+    });
+@endphp
+
 <div class="relative" x-data="{
-    images: [
-      'https://api.stroom.id/v1/file/asset/load/jpg/fo9h2ynulu',
-      'https://api.stroom.id/v1/file/asset/load/jpg/u7wwwpox6y',
-    ],
+    images: {{ $carousel }},
     active: 0,
+    changeIndex(){
+        this.active++
+        if (this.active === this.images.length) {
+            this.active = 0;
+        }
+    },
     loop() {
         setInterval(() => {
-            this.active == 0 ?  this.active = + 1 : this.active = 0
+            this.changeIndex()
         }, 5000)
     }
 }" x-init="loop">
     <div class="lg:h-[80vh] w-full bg-brands-primary/80 overflow-hidden">
-        <img x-bind:src="images[active]" alt="" class="aspect-video lg:h-full lg:w-full">
+        <img x-bind:src="images[active]['path']" alt="" class="aspect-video lg:h-full lg:w-full">
         <div class="w-full h-full absolute px-5 lg:px-10 z-10 top-0 flex flex-col justify-center">
             <div class="w-full flex justify-between items-center -mt-20 lg:md-0">
                 <button @click="active > 0 ? active = active - 1 : active = 0" class="p-2 md:p-3 bg-white/80 hover:bg-white rounded-full">

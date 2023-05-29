5ee84aadedf38e3c4d2ae8f6e531093a12eeaae7
msbarf14 <misbahularifin14@gmail.com>
2023-05-29 23:44:27 +0800

    wip products

--- database/migrations/2023_05_29_154235_add_column_to_products.php
+++ database/migrations/2023_05_29_154235_add_column_to_products.php
@@ -14,17 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::table('products', function (Blueprint $table) {
-<<<<<<< HEAD
-            $table->string('name');
-            $table->integer('type');
-=======
-            $table->string('title')
-                ->nullable()
-                ->after('id');
-            $table->json('meta')
-                ->nullable()
-                ->after('status');
->>>>>>> 3fbc52c (wip products)
+            //
         });
     }
 
@@ -36,12 +26,7 @@ return new class extends Migration
     public function down()
     {
         Schema::table('products', function (Blueprint $table) {
-<<<<<<< HEAD
-            $table->dropColumn(['name','type']);
-=======
-            $table->dropColumn('title');
-            $table->dropColumn('meta');
->>>>>>> 3fbc52c (wip products)
+            //
         });
     }
 };

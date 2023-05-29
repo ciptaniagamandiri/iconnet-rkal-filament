e13b67e3339fba2dbad07f66e84f83a08055923a
msbarf14 <misbahularifin14@gmail.com>
2023-05-30 00:56:50 +0800

    improve menu navbar

--- resources/views/landing/components/navbar.blade.php
+++ resources/views/landing/components/navbar.blade.php
@@ -6,12 +6,12 @@
                     <img class="block h-8 w-auto" src="/assets/icon-logo.png" alt="Your Company">
                 </a>
                 <div class="hidden sm:ml-6 sm:block">
-                    <div class="flex space-x-4">
+                    <div class="flex space-x-4 items-center">
                         <a href="/" class="rounded-md px-3 py-2 text-sm font-medium  hover:text-brands-secondary {{request()->routeIs('landing') ? 'text-brands-secondary' : 'text-white'}}">Home</a>
                         <a href="/products" class="rounded-md px-3 py-2 text-sm font-medium {{request()->routeIs('landing.product') ? 'text-brands-secondary' : 'text-white'}} hover:text-brands-secondary">Produk & Promo </a>
                         <a href="/area" class="rounded-md px-3 py-2 text-sm font-medium {{request()->routeIs('landing.area') ? 'text-brands-secondary' : 'text-white'}} hover:text-brands-secondary">Cakupan Area</a>
                         <a href="/contact" class="rounded-md px-3 py-2 text-sm font-medium {{request()->routeIs('landing.contact') ? 'text-brands-secondary' : 'text-white'}} hover:text-brands-secondary">Hubungi kami</a>
-                        <a href="#" class="block rounded-md px-3 py-2 text-base font-medium text-gray-300  hover:bg-gray-700 hover:text-white">Mitra Penjualan</a>
+                        <a href="#" class="block rounded-md px-3 py-2 text-base font-medium text-white bg-brands-secondary  hover:bg-sky-100 hover:text-brands-secondary hover:transition hover:duration-300 hover:ease-in">Mitra Penjualan</a>
                     </div>
                 </div>
             </div>
@@ -37,8 +37,7 @@
             <a href="/products" class="block rounded-md px-3 py-2 text-base font-medium text-gray-300 {{ request()->routeIs('landing.product') ? 'bg-gray-900' : '' }} hover:bg-gray-700 hover:text-white">Produk & Promo</a>
             <a href="/area" class="block rounded-md px-3 py-2 text-base font-medium text-gray-300 {{ request()->routeIs('landing.area') ? 'bg-gray-900' : '' }} hover:bg-gray-700 hover:text-white">Cakupan Area</a>
             <a href="/contact" class="block rounded-md px-3 py-2 text-base font-medium text-gray-300 {{ request()->routeIs('landing.contact') ? 'bg-gray-900' : '' }} hover:bg-gray-700 hover:text-white">Hubungi kami</a>
-            <a href="/contact" class="rounded-md px-3 py-2 text-sm font-medium {{request()->routeIs('landing.contact') ? 'text-brands-secondary' : 'text-white'}} hover:text-brands-secondary">Hubungi kami</a>
-            <a href="#" class="block rounded-md px-3 py-2 text-base font-medium text-gray-300  hover:bg-gray-700 hover:text-white">Mitra Penjualan</a>
+            <a href="#" class="block rounded-md px-3 py-2 text-base font-medium text-white bg-brands-secondary  hover:bg-sky-100 hover:text-brands-secondary hover:transition hover:duration-300 hover:ease-in">Mitra Penjualan</a>
         </div>
     </div>
 </nav>

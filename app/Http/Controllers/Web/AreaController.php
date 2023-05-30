08ae3bba0364445e859455fda1ac4a88868075f1
nickodaken <nickodaken24@gmail.com>
2023-05-31 00:10:22 +0800

    data area coverage with params

--- app/Http/Controllers/Web/AreaController.php
+++ app/Http/Controllers/Web/AreaController.php
@@ -8,11 +8,22 @@ use App\Models\Province;
 use App\Models\Regency;
 use App\Models\District;
 use App\Models\Coverage\Map;
+use App\Models\Coverage\Area;
 class AreaController extends Controller
 {
     public function index()
     {
-        $data =  Province::whereIn('id', [61,62,63,64,65])->with('map', 'area')->get();
-        return view('landing.area', compact('data'));
+        $provinsi = request()->province_id;
+        $kota = request()->regency_id;
+        $kecamatan = request()->district_id;
+        $kelurahan = request()->village_id;
+
+        $areaCoverage = Area::where('province_id', $provinsi)
+            ->where('regency_id', $kota)
+            ->where('district_id', $kecamatan)
+            ->where('village_id', $kelurahan)
+            ->get();
+        return $areaCoverage;
+        return view('landing.area', compact('areaCoverage'));
     }
 }

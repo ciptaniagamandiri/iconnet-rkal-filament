fa0004d3d696df90a6bd33455609ff7d8a27659c
nickodaken <nickodaken24@gmail.com>
2023-06-08 01:39:37 +0800

    data api Area

--- app/Http/Controllers/Web/AreaController.php
+++ app/Http/Controllers/Web/AreaController.php
@@ -3,27 +3,14 @@
 namespace App\Http\Controllers\Web;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
-use App\Models\Province;
-use App\Models\Regency;
-use App\Models\District;
-use App\Models\Coverage\Map;
 use App\Models\Coverage\Area;
+
 class AreaController extends Controller
 {
     public function index()
     {
-        $provinsi = request()->province_id;
-        $kota = request()->regency_id;
-        $kecamatan = request()->district_id;
-        $kelurahan = request()->village_id;
+        $areaCoverage = Area::get()->groupBy(['province.name', 'regency.name', 'district.name', 'village.name']);
 
-        $areaCoverage = Area::where('province_id', $provinsi)
-            ->where('regency_id', $kota)
-            ->where('district_id', $kecamatan)
-            ->where('village_id', $kelurahan)
-            ->get();
-            
         return view('landing.area', compact('areaCoverage'));
     }
 }
--- app/Models/Village.php
+++ app/Models/Village.php
@@ -12,6 +12,7 @@ namespace App\Models;
 use AzisHapidin\IndoRegion\Traits\VillageTrait;
 use Illuminate\Database\Eloquent\Model;
 use App\Models\District;
+use App\Models\Coverage\Area;
 
 /**
  * Village Model.
@@ -36,7 +37,7 @@ class Village extends Model
         'district_id'
     ];
 
-	/**
+    /**
      * Village belongs to District.
      *
      * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
@@ -44,5 +45,10 @@ class Village extends Model
     public function district()
     {
         return $this->belongsTo(District::class);
+    }
+
+    public function areas()
+    {
+        return $this->hasMany(Area::class);
     }
 }

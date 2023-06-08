6d6b7238bec7d0e8812f4dc371de9a22d0b771ab
nickodaken <nickodaken24@gmail.com>
2023-06-08 23:07:54 +0800

    delete route api

--- routes/api.php
+++ routes/api.php
@@ -14,8 +14,6 @@ use App\Http\Controllers\Web;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-Route::get('/test', [Web\AreaController::class, 'index']);
-
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });

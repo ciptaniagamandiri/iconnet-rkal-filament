b638987ca743eea67332a81103e2c60fdb60cc10
nickodaken <nickodaken24@gmail.com>
2023-06-11 21:29:20 +0800

    data area in landing

--- app/Http/Controllers/Web/AreaController.php
+++ app/Http/Controllers/Web/AreaController.php
@@ -15,18 +15,28 @@ class AreaController extends Controller
         $kelurahan = request()->village_id;
 
         $filter = compact('provinsi', 'kota', 'kecamatan', 'kelurahan');
-        if (($filter)) {
-            $areaCoverage = Area::with('province', 'regency', 'district', 'village')
-                ->where('province_id', 'LIKE', '%' . $provinsi . '%')
-                ->where('regency_id', 'LIKE', '%' . $kota . '%')
-                ->where('district_id', 'LIKE', '%' . $kecamatan . '%')
-                ->where('village_id', 'LIKE', '%' . $kelurahan . '%')
-                ->get()
-                ->groupBy(['province.name', 'regency.name', 'district.name', 'village.name']);
-        } else {
-            $areaCoverage = Area::get()->groupBy(['province.name', 'regency.name', 'district.name', 'village.name']);
-        }
-        return $areaCoverage;
+        // if (($filter)) {
+        //     $areaCoverage = Area::with('province', 'regency', 'district', 'village')
+        //         ->where('province_id', 'LIKE', '%' . $provinsi . '%')
+        //         ->where('regency_id', 'LIKE', '%' . $kota . '%')
+        //         ->where('district_id', 'LIKE', '%' . $kecamatan . '%')
+        //         ->where('village_id', 'LIKE', '%' . $kelurahan . '%')
+        //         ->get()
+        //         ->mapToGroups(function ($item) {
+        //             return [$item['province']['name'] => [$item['regency']['name'] => [$item]]];
+        //         })->all();
+        // } else {
+        //     $areaCoverage = Area::where('status', true)->paginate(10)->mapToGroups(function ($item) {
+        //         return [[$item['province_id'] => [$item['regency_id'] => [$item]]]];
+        //     })->all();
+        // }
+        // foreach ($areaCoverage as $key => $value) {
+        //     foreach ($value as $key => $value) {
+        //         return $value;
+        //     }
+        // }
+        $areaCoverage = Area::get()->groupBy(['province.name', 'regency.name', 'district.name', 'village.name']);
+        // return $areaCoverage;
         return view('landing.area', compact('areaCoverage'));
     }
 }

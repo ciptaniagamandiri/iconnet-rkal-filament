90db345aa185b35517c762127baddf73e5f0245d
nickodaken <nickodaken24@gmail.com>
2023-06-12 22:49:09 +0800

    filter area coverage without dynamical select

--- app/Http/Controllers/Web/AreaController.php
+++ app/Http/Controllers/Web/AreaController.php
@@ -3,16 +3,69 @@
 namespace App\Http\Controllers\Web;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Models\Coverage\Area;
+use App\Models\District;
 use App\Models\Province;
 use App\Models\Regency;
-use App\Models\District;
-use App\Models\Coverage\Map;
+use App\Models\Village;
+
 class AreaController extends Controller
 {
     public function index()
     {
-        $data =  Province::whereIn('id', [61,62,63,64,65])->with('map', 'area')->get();
-        return view('landing.area', compact('data'));
+        $provinsi = request()->province_id;
+        $kota = request()->regency_id;
+        $kecamatan = request()->district_id;
+        $kelurahan = request()->village_id;
+
+        $dataProvinsi = Province::whereIn('id', [61, 62, 63, 64, 65])->get();
+        $dataKota = Regency::where('province_id', $provinsi)->get();
+        $dataKecamatan = District::where('regency_id', $kota)->get();
+        $dataKelurahan = Village::where('district_id', $kecamatan)->get();
+
+        $areaCoverage = [];
+        if ($provinsi) {
+            $areaCoverage = Area::with('province', 'regency', 'district', 'village')
+                ->where('province_id', 'LIKE', '%' . $provinsi . '%')
+                ->where('regency_id', 'LIKE', '%' . $kota . '%')
+                ->where('district_id', 'LIKE', '%' . $kecamatan . '%')
+                ->where('village_id', 'LIKE', '%' . $kelurahan . '%')
+                ->get()
+                ->groupBy(['province.name', 'regency.name', 'district.name', 'village.name']);
+        } else {
+            $areaCoverage = Area::get()->groupBy(['province.name', 'regency.name', 'district.name', 'village.name']);
+        }
+        // return response()->json($areaCoverage);
+        return view('landing.area', compact('areaCoverage', 'dataProvinsi', 'dataKota', 'dataKecamatan', 'dataKelurahan'));
+    }
+
+    public function fetchDataKota()
+    {
+        $provinsi = request()->province_id;
+        $kota = [];
+        if ($provinsi) {
+            $kota = Regency::where('province_id', $provinsi)->get();
+        }
+        return response()->json($kota);
+    }
+
+    public function fetchDataKecamatan()
+    {
+        $kota = request()->regency_id;
+        $kecamatan = [];
+        if ($kota) {
+            $kecamatan = District::where('regency_id', $kota)->get();
+        }
+        return response()->json($kecamatan);
+    }
+
+    public function fetchDataKelurahan()
+    {
+        $kecamatan = request()->district_id;
+        $kelurahan = [];
+        if ($kecamatan) {
+            $kelurahan = Village::where('district_id', $kecamatan)->get();
+        }
+        return response()->json($kelurahan);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\Web;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,12 +17,15 @@ use App\Http\Controllers\Web;
 
 Route::get('/', [Web\LandingController::class, 'index'])->name('landing');
 Route::get('/area', [Web\AreaController::class, 'index'])->name('landing.area');
-Route::get('/products', function(){
+Route::get('/products', function () {
     return view('landing.product');
 })->name('landing.product');
-Route::get('/contact', function(){
+Route::get('/contact', function () {
     return view('landing.contact');
 })->name('landing.contact');
 Route::post('/form', [Web\FormregistrationController::class, 'form'])->name('form.store');
+Route::post('/send-otp', [Web\FormregistrationController::class, 'otp'])->name('form.otp');
 
-
+Route::get('fetch/data/kota', [Web\AreaController::class, 'fetchDataKota'])->name('fetch.data.kota');
+Route::get('fetch/data/kecamatan', [Web\AreaController::class, 'fetchDataKecamatan'])->name('fetch.data.kecamatan');
+Route::get('fetch/data/kelurahan', [Web\AreaController::class, 'fetchDataKelurahan'])->name('fetch.data.kelurahan');

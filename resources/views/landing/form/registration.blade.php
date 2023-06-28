87657830f714d6a6148f78e6d650cac481c90f50
Misbahul Arifin <misbahularifin14@gmail.com>
2023-06-28 14:16:58 +0800

    split file js

--- resources/views/landing/form/registration.blade.php
+++ resources/views/landing/form/registration.blade.php
@@ -157,6 +157,7 @@
 @section('script')
   <script src="https://cdn.jsdelivr.net/npm/leaflet@1.7.1/dist/leaflet.js"></script>
   <script src="https://unpkg.com/leaflet-control-geocoder/dist/Control.Geocoder.js"></script>
+  <script src="/assets/js/leaflet.js"></script>
 
   <script>
     document.addEventListener('alpine:init', () => {
@@ -173,51 +174,5 @@
         // }))
 
     })
-    // Initialize the map
-    // Initialize the map
-    var map = L.map("map", {
-      zoomControl: false 
-    }).setView([-1.241659, 116.853412], 13);
-
-      // Create the tile layer with your preferred map provider
-      L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-        attribution: "&copy; OpenStreetMap contributors",
-      }).addTo(map);
-
-      // Create a marker with a draggable option
-      var marker = L.marker([-1.241659, 116.853412], { draggable: true }).addTo(map);
-
-      // Function to update the latitude and longitude inputs
-      function updateLatLng() {
-        var latLng = marker.getLatLng();
-        document.getElementById("latitude").value = latLng.lat.toFixed(6);
-        document.getElementById("longitude").value = latLng.lng.toFixed(6);
-      }
-
-      // Update the inputs when the marker is dragged
-      marker.on("drag", updateLatLng);
-
-      // Initialize the geocoder control
-      var geocoder = L.Control.geocoder({
-        collapsed: false,
-        placeholder: "Cari lokasi..."
-      }).addTo(map);
-
-      // Function to handle the geocode result
-      function handleResult(result) {
-        if (result || result.length > 0) {
-          var location = result.center;
-          marker.setLatLng(location);
-          map.setView(location, 13);
-          console.log(result);
-          updateLatLng()
-        }
-      }
-
-      // Listen for geocode result event
-      geocoder.on("markgeocode", function (e) {
-        handleResult(e.geocode);
-      });
-
   </script>
 @endsection
